ccfd1ebcd4a24e30a00e7afa92638801ea9fa621
Frederic Brodbeck <der.froedi@gmail.com>
2023-11-19 16:37:01 +0800

    concurrently fetch feeds

--- main.go
+++ main.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"sync"
+)
+
+func readFeedUrls() map[string]string {
+	const filePath = "../../feeds.json"
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error opening file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	var data map[string]string
+	err = decoder.Decode(&data)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error decoding JSON:", err)
+		os.Exit(1)
+	}
+	return data
+}
+
+func requestFeed(url string) string {
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error getting feed:", err)
+		return ""
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading body:", err)
+		return ""
+	}
+
+	return string(body)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	feedsMap := readFeedUrls()
+
+	var wg sync.WaitGroup
+
+	// create a buffered channel (which acts as a semaphone)
+	// to control concurrency
+	maxConcurrency := 2
+	// struct{} is an empty struct, which takes up no memory
+	sem := make(chan struct{}, maxConcurrency)
+
+	var results = make(map[string]string)
+
+	for name, url := range feedsMap {
+		wg.Add(1)
+		go func(name string, url string) {
+			sem <- struct{}{} // blocks if channel is full
+			defer wg.Done()
+			fmt.Println(name)
+			content := requestFeed(url)
+			results[name] = content
+			<-sem // release
+		}(name, url)
+	}
+
+	wg.Wait()
 }
