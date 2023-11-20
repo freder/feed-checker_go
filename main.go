5b14fb1118d7fc708a212a23ad673aeaac3d605d
Frederic Brodbeck <der.froedi@gmail.com>
2023-11-20 10:45:07 +0800

    check for new items since last check and print their titles

--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 	"sync"
+	"time"
+
+	"github.com/mmcdole/gofeed"
 )
+
+const lastCheckTimeFilePath = "./last-check.txt"
 
 func readFeedUrls() map[string]string {
 	const filePath = "../../feeds.json"
@@ -49,6 +55,42 @@ func requestFeed(url string) string {
 func main() {
 	feedsMap := readFeedUrls()
 
+	var lastCheckTime time.Time
+
+	if _, err := os.Stat(lastCheckTimeFilePath); os.IsNotExist(err) {
+		lastCheckTime = time.Date(1, 1, 1, 0, 0, 0, 0, time.Local)
+	} else {
+		bytes, err := os.ReadFile(lastCheckTimeFilePath)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading file:", err)
+			os.Exit(1)
+		}
+		parsed, err := time.Parse(time.RFC3339, string(bytes))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error parsing time:", err)
+			os.Exit(1)
+		}
+		lastCheckTime = parsed
+	}
+
+	now := time.Now()
+
+	// for testing
+	lastCheckTime = time.Date(2023, 6, 1, 0, 0, 0, 0, time.Local)
+
+	// write current time to timeFile
+	timeFile, err := os.Create(lastCheckTimeFilePath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating file:", err)
+		os.Exit(1)
+	}
+	defer timeFile.Close()
+	_, err = timeFile.WriteString(now.Format(time.RFC3339))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error writing to file:", err)
+		os.Exit(1)
+	}
+
 	var wg sync.WaitGroup
 
 	// create a buffered channel (which acts as a semaphone)
@@ -57,19 +99,55 @@ func main() {
 	// struct{} is an empty struct, which takes up no memory
 	sem := make(chan struct{}, maxConcurrency)
 
-	var results = make(map[string]string)
+	var results = make(map[string][]*gofeed.Item)
 
 	for name, url := range feedsMap {
 		wg.Add(1)
 		go func(name string, url string) {
 			sem <- struct{}{} // blocks if channel is full
 			defer wg.Done()
-			fmt.Println(name)
+
+			// fmt.Println(name)
 			content := requestFeed(url)
-			results[name] = content
+
+			// parse feed
+			parser := gofeed.NewParser()
+			feed, err := parser.ParseString(content)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error parsing feed:", err)
+				return
+			}
+
+			filtered := make([]*gofeed.Item, 0)
+
+			for _, item := range feed.Items {
+				if item.PublishedParsed.After(lastCheckTime) {
+					filtered = append(filtered, item)
+				}
+			}
+
+			results[name] = filtered
+
 			<-sem // release
 		}(name, url)
 	}
 
 	wg.Wait()
+
+	for name, items := range results {
+		c := len(items)
+		if c == 0 {
+			continue
+		}
+		fmt.Println()
+		fmt.Println(name + ": " + fmt.Sprint(c))
+		for _, item := range items {
+			timestamp := item.PublishedParsed.Format(time.RFC3339)
+			timestamp = strings.Split(timestamp, "T")[0]
+			fmt.Println(
+				"-", item.Title,
+				fmt.Sprintf("(%s)", timestamp),
+			)
+		}
+	}
 }
