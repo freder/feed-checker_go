047956b063a4dbf1ad49db60b187f9dc7533bbc2
Frederic Brodbeck <der.froedi@gmail.com>
2023-11-20 10:50:39 +0800

    refactor

--- main.go
+++ main.go
@@ -15,6 +15,42 @@ import (
 
 const lastCheckTimeFilePath = "./last-check.txt"
 
+func updateLastCheckTimeFile(now time.Time) {
+	timeFile, err := os.Create(lastCheckTimeFilePath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating file:", err)
+		os.Exit(1)
+	}
+	defer timeFile.Close()
+	_, err = timeFile.WriteString(now.Format(time.RFC3339))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error writing to file:", err)
+		os.Exit(1)
+	}
+}
+
+func getLastCheckTime() time.Time {
+	var lastCheckTime time.Time
+
+	if _, err := os.Stat(lastCheckTimeFilePath); os.IsNotExist(err) {
+		lastCheckTime = time.Date(1, 1, 1, 0, 0, 0, 0, time.Local)
+	} else {
+		bytes, err := os.ReadFile(lastCheckTimeFilePath)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading file:", err)
+			os.Exit(1)
+		}
+		parsed, err := time.Parse(time.RFC3339, string(bytes))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error parsing time:", err)
+			os.Exit(1)
+		}
+		lastCheckTime = parsed
+	}
+
+	return lastCheckTime
+}
+
 func readFeedUrls() map[string]string {
 	const filePath = "../../feeds.json"
 
@@ -55,41 +91,13 @@ func requestFeed(url string) string {
 func main() {
 	feedsMap := readFeedUrls()
 
-	var lastCheckTime time.Time
-
-	if _, err := os.Stat(lastCheckTimeFilePath); os.IsNotExist(err) {
-		lastCheckTime = time.Date(1, 1, 1, 0, 0, 0, 0, time.Local)
-	} else {
-		bytes, err := os.ReadFile(lastCheckTimeFilePath)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading file:", err)
-			os.Exit(1)
-		}
-		parsed, err := time.Parse(time.RFC3339, string(bytes))
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error parsing time:", err)
-			os.Exit(1)
-		}
-		lastCheckTime = parsed
-	}
-
 	now := time.Now()
-
-	// for testing
-	lastCheckTime = time.Date(2023, 6, 1, 0, 0, 0, 0, time.Local)
+	lastCheckTime := getLastCheckTime()
+	// TODO: remove — for testing only
+	// lastCheckTime = time.Date(2023, 6, 1, 0, 0, 0, 0, time.Local)
 
 	// write current time to timeFile
-	timeFile, err := os.Create(lastCheckTimeFilePath)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error creating file:", err)
-		os.Exit(1)
-	}
-	defer timeFile.Close()
-	_, err = timeFile.WriteString(now.Format(time.RFC3339))
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error writing to file:", err)
-		os.Exit(1)
-	}
+	updateLastCheckTimeFile(now)
 
 	var wg sync.WaitGroup
 
@@ -134,20 +142,27 @@ func main() {
 
 	wg.Wait()
 
+	newItemsCount := 0
 	for name, items := range results {
 		c := len(items)
 		if c == 0 {
 			continue
 		}
+		newItemsCount += c
 		fmt.Println()
 		fmt.Println(name + ": " + fmt.Sprint(c))
 		for _, item := range items {
 			timestamp := item.PublishedParsed.Format(time.RFC3339)
 			timestamp = strings.Split(timestamp, "T")[0]
 			fmt.Println(
-				"-", item.Title,
+				"-",
 				fmt.Sprintf("(%s)", timestamp),
+				item.Title,
 			)
 		}
+	}
+
+	if newItemsCount == 0 {
+		fmt.Println("No new items")
 	}
 }
